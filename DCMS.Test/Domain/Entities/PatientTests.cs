a8978ea7233f199f05aa33673e10635527e12ae2
isoboye24 <isobarguy4real@gmail.com>
2026-06-23 13:09:53 +0200

    Patient entity tested

---
+++ DCMS.Test/Domain/Entities/PatientTests.cs
@@ -0,0 +1,59 @@
+﻿using DCMS.Domain.Entities;
+using DCMS.Domain.Exceptions;
+using DCMS.Domain.ValueObjects;
+
+namespace DCMS.Test.Domain.Entities
+{
+    [TestClass]
+    public class PatientTests
+    {
+        [TestMethod]
+        public void Constructor_NullName_ThrowsBusinessRuleException()
+        {
+            var email = new Email("iso@gmail.com");
+
+            var ex = Assert.Throws<BusinessRuleException>(
+                () => new Patient(null!, email));
+
+            Assert.AreEqual(
+                "Name is required.",
+                ex.Message);
+        }
+
+        [TestMethod]
+        public void Constructor_NullEmail_ThrowsBusinessRuleException()
+        {
+            var ex = Assert.Throws<BusinessRuleException>(
+                () => new Patient("Vincent", email: null!));
+
+            Assert.AreEqual(
+                "Email is required.",
+                ex.Message);
+        }
+
+        [TestMethod]
+        public void Constructor_EmptyName_ThrowsBusinessRuleException()
+        {
+            var email = new Email("iso@gmail.com");
+
+            var ex = Assert.Throws<BusinessRuleException>(
+                () => new Patient(string.Empty, email));
+
+            Assert.AreEqual("Name is required.", ex.Message);
+        }
+
+        [TestMethod]
+        public void Constructor_ValidDentist_CreatesDentist()
+        {
+            // Arrange
+            var email = new Email("iso@gmail.com");
+
+            // Act
+            var dentist = new Patient("Vincent", email);
+
+            // Assert
+            Assert.AreEqual("Vincent", dentist.Name);
+            Assert.AreEqual(email, dentist.Email);
+        }
+    }
+}

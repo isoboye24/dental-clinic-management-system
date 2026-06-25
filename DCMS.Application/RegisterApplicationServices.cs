a4278e2c5c02e3cfd51e394ef8990f889f78add2
isoboye24 <isobarguy4real@gmail.com>
2026-06-25 13:47:49 +0200

    Application service layer registered.

---
+++ DCMS.Application/RegisterApplicationServices.cs
@@ -0,0 +1,22 @@
+﻿using DCMS.Application.Features.DentalOffices.Commands.CreateDentalOffice;
+using DCMS.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using DCMS.Application.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DCMS.Application
+{
+    public static class RegisterApplicationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Register application services here
+
+            services.AddTransient<IMediator, SimpleMediator>();
+
+            services.AddScoped<IRequestHandler<CreateDentalOfficeCommand, Guid>, CreateDentalOfficeCommandHandler>();
+            services.AddScoped<IRequestHandler<GetDentalOfficeDetailQuery, DentalOfficeDetailDTO>, GetDentalOfficeDetailQueryHandler>();
+
+            return services;
+        }
+    }
+}

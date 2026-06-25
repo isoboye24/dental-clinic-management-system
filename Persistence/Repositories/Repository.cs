c3b6249a1d972a55257609d9f414296ed71c918d
isoboye24 <isobarguy4real@gmail.com>
2026-06-25 14:23:23 +0200

    Repository created

---
+++ Persistence/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using DCMS.Application.Contracts.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DCMSDBContext _db;
+
+        public Repository(DCMSDBContext db)
+        {
+            _db = db;
+        }
+
+        public Task<T> Add(T entity)
+        {
+            _db.Add(entity);
+            return Task.FromResult(entity);
+        }
+
+        public Task Delete(T entity)
+        {
+            _db.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _db.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetById(Guid id)
+        {
+            return await _db.Set<T>().FindAsync(id);
+        }
+
+        public Task Update(T entity)
+        {
+            _db.Update(entity);
+            return Task.CompletedTask;
+        }
+    }
+}

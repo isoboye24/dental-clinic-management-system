198a5fbba2c6d2f45b807ef24db8599540967003
isoboye24 <isobarguy4real@gmail.com>
2026-06-25 13:26:59 +0200

    Unit Test on GetDentalOfficeCommandHandler implemented.

---
+++ DCMS.Test/Application/Features/DentalOffices/GetDentalOfficeCommandHandlerTest.cs
@@ -0,0 +1,40 @@
+﻿using DCMS.Application.Contracts.Repositories;
+using DCMS.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using DCMS.Domain.Entities;
+using NSubstitute;
+
+namespace DCMS.Test.Application.Features.DentalOffices
+{
+    [TestClass]
+    public class GetDentalOfficeCommandHandlerTest
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring the field as nullable.
+        private IDentalOfficeRepository _repository;
+        private GetDentalOfficeDetailQueryHandler _handler;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring the field as nullable.
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repository = Substitute.For<IDentalOfficeRepository>();
+            _handler = new GetDentalOfficeDetailQueryHandler(_repository);
+        }
+
+        [TestMethod]
+        public async Task Handle_DentalOfficeExists_ReturnsIt()
+        {
+            var dentalOffice = new DentalOffice("Test Dental Office");
+            var id = dentalOffice.Id;
+            var query = new GetDentalOfficeDetailQuery { Id = id};
+
+            _repository.GetById(id).Returns(dentalOffice);
+
+            var result = await _handler.Handle(query);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(id, result.Id);
+            Assert.AreEqual(dentalOffice.Name, result.Name);
+        }
+    }
+}

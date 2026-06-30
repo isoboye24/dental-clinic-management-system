28e524a43f20546948d0f5be7ada5a7719256ad1
isoboye24 <isobarguy4real@gmail.com>
2026-06-30 13:36:15 +0200

    DentalOfficeController created with Create and GetByid actions implemented

---
+++ DCMS.API/Controllers/DentalOfficesController.cs
@@ -0,0 +1,43 @@
+﻿using DCMS.API.DTOs.DentalOffices;
+using DCMS.Application.Features.DentalOffices.Commands.CreateDentalOffice;
+using DCMS.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using DCMS.Application.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DCMS.API.Controllers
+{
+    [ApiController]
+    [Route("api/dentaloffices")]
+    public class DentalOfficesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DentalOfficesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateDentalOfficeDTO createDentalOfficeDTO)
+        {
+            var command = new CreateDentalOfficeCommand { Name = createDentalOfficeDTO.Name};
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id },
+                null);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DentalOfficeDetailDTO>> GetById(Guid id)
+        {
+            var query = new GetDentalOfficeDetailQuery { Id = id };
+            var result = await _mediator.Send(query);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+}
+}
---
+++ DCMS.API/DTOs/DentalOffices/CreateDentalOfficeDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DCMS.API.DTOs.DentalOffices
+{    
+    public class CreateDentalOfficeDTO
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters.")]
+        public required string Name { get; set; }
+    }
+}

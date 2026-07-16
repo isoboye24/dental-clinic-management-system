0192ae3fc949d6ffb2184473c8b8f156d41a78a3
isoboye24 <isobarguy4real@gmail.com>
2026-07-16 12:03:12 +0200

    Dentist repository created

---
+++ DCMS.Application/Features/Dentists/Queries/GetDentistsList/DentistFilterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DCMS.Application.Features.Dentists.Queries.GetDentistsList
+{
+    public class DentistFilterDTO
+    {
+        public int Page { get; set; } = 1;
+        public int RecordsPerPage { get; set; } = 10;
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ Persistence/Repositories/DentistRepository.cs
@@ -0,0 +1,38 @@
+﻿using DCMS.Application.Contracts.Repositories;
+using DCMS.Application.Features.Dentists.Queries.GetDentistsList;
+using DCMS.Domain.Entities;
+using DCMS.Persistence.Utilities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DCMS.Persistence.Repositories
+{
+    public class DentistRepository : Repository<Dentist>, IDentistRepository
+    {
+        private readonly DCMSDBContext _db;
+        public DentistRepository(DCMSDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Dentist>> GetFiltered(DentistFilterDTO
+            filter)
+        {
+            var query = _db.Dentists.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(filter.Name))
+            {
+                query = query.Where(p => p.Name.Contains(filter.Name));
+            }
+
+            if (!string.IsNullOrWhiteSpace(filter.Email))
+            {
+                query = query.Where(p => p.Email.Value.Contains(filter.Email));
+            }
+
+            return await query
+                .OrderBy(x => x.Name)
+                .Paginate(filter.Page, filter.RecordsPerPage)
+                .ToListAsync();
+        }
+    }
+}

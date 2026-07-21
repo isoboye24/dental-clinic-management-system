c746a81032ed0ce9e53714bed3d557a8f40d3f1a
isoboye24 <isobarguy4real@gmail.com>
2026-07-22 01:00:34 +0200

    Appointment repository created.

---
+++ DCMS.Application/Contracts/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,8 @@
+﻿using DCMS.Domain.Entities;
+
+namespace DCMS.Application.Contracts.Repositories
+{
+    public interface IAppointmentRepository : IRepository<Appointment>
+    {
+    }
+}
---
+++ Persistence/Repositories/AppointmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using DCMS.Application.Contracts.Repositories;
+using DCMS.Domain.Entities;
+
+namespace DCMS.Persistence.Repositories
+{
+    public class AppointmentRepository : Repository<Appointment>, IAppointmentRepository
+    {
+        public AppointmentRepository(DCMSDBContext db) : base(db)
+        {
+           
+        }
+    
+    }
+}
